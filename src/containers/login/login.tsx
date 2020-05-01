96327ed8a068cb34b1a448c23730c3200e220bfb
Umberto Ciriello <umberto.ciriello@quinck.io>
2020-05-01 18:20:45 +0200

    login design

--- src/containers/login/login.tsx
+++ src/containers/login/login.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
 import './login.css';
+import logo from '../../assets/logo.png'
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 interface LoginProps {
   state?: string;
@@ -14,7 +17,40 @@ class Login extends React.Component<
   LoginState
 > {
   public render() {
-    return <div>Login</div>;
+    return (
+      <div className="login-content">
+        <div className="login-title">
+          <img className="login-logo" src={logo} alt="Logo" /> uinck Dashboard
+          Login
+        </div>
+        <div className="login-area">
+          <div className="login-area-form">
+            <div className="login-field">
+              <TextField
+                id="username-field"
+                label="Username"
+                autoComplete="current-username"
+                variant="outlined"
+              />
+            </div>
+            <div className="login-field">
+              <TextField
+                id="password-field"
+                label="Password"
+                type="password"
+                autoComplete="current-password"
+                variant="outlined"
+              />
+            </div>
+            <div className="login-field">
+              <Button variant="contained" color="primary">
+                Login
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
